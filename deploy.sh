aea3bd628aeb25b08ca3c2694be2c9a6627ae6d3
kwasiasare <kwasi.spreadcom@gmail.com>
2025-04-20 22:17:32 -0400

    created deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+STACK_NAME="MyInfraStack"
+TEMPLATE_FILE="cloud_infra_stack.yaml"
+REGION="us-east-2"
+
+echo "Deploying CloudFormation stack: $STACK_NAME"
+
+aws cloudformation deploy \
+  --stack-name $STACK_NAME \
+  --template-file $TEMPLATE_FILE \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --region $REGION \
+  --parameter-overrides \
+    VpcCIDR=10.0.0.0/16 \
+    PublicSubnet1CIDR=10.0.1.0/24 \
+    PublicSubnet2CIDR=10.0.2.0/24 \
+    PrivateSubnet1CIDR=10.0.3.0/24 \
+    PrivateSubnet2CIDR=10.0.4.0/24
+
+echo "Deployment complete."
